001a037d440f5eeb2a94069414079896d8b4886e
opssoposs <opssoposs@naver.com>
2024-01-10 12:29:37 +0900

    #2 에셋추가, Enemy.cs수정

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    int EnemyHp;
+    int EnemyHp; //현재 적 Hp
+    [SerializeField] int EnemyHpSetting; //기본 Hp
+    int BulletPower;
     public void Awake()
     {
-        EnemyHp = 10; //EnemyHP 초기값 세팅
         
+        EnemyHp = EnemyHpSetting; //EnemyHP 초기값 세팅
     }
-    void Update()
+    private void Update()
     {
-        HP();
+        HPsystem();
     }
-    public void HP() 
+    private void HPsystem() //HP
     {
         if (EnemyHp <= 0)//HP가 0이 되었을때 게임오브젝트 파괴
         {
             Destroy(gameObject);
         }
+    }
+    private void EnemyHit() //피격 이벤트
+    {
+        if (CompareTag("Bullet")) //태그가 Bullet라면 EnemyHp가 BulletPower만큼 닳고 디버그 로그에 Hit를 띄움
+        {
+            EnemyHp = EnemyHp - BulletPower;
+            Debug.Log("HIt");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        EnemyHit();
     }
 }
